f0a2f174b374d0e757d51585ba36e7815716544e
Институт <laktionova-99@mail.ru>
2018-12-21 12:02:31 +0300

    Zachot old version

--- src/peresdacha/Question.java
+++ src/peresdacha/Question.java
@@ -1,4 +1,25 @@
 package peresdacha;
 
 public class Question {
+    private int x;
+    private int y;
+    private String questionString;
+
+    Question(int x, int y, String questionString) {
+        this.x = x;
+        this.y = y;
+        this.questionString = questionString;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getQuestionString() {
+        return questionString;
+    }
 }
--- src/peresdacha/Zachot.java
+++ src/peresdacha/Zachot.java
@@ -3,14 +3,16 @@ package peresdacha;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import ljavafx.L7_ResourcesExamples;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.io.InputStream;
+import java.util.Scanner;
 
 
 public class Zachot extends Application {
@@ -26,23 +28,68 @@ public class Zachot extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        primaryStage.setTitle("Ослиные вопросы");
+        primaryStage.setTitle("Собачьи вопросы");
 
         Parent root = initInterface();
         primaryStage.setScene(new Scene(root));
 
+        initInteraction(primaryStage);
         primaryStage.show();
     }
 
     private Parent initInterface() {
-        VBox root = new VBox();
+        /*VBox root = new VBox();
 
         InputStream pickURL = L7_ResourcesExamples.class.getResourceAsStream("ослик.jpg");
         Image img = new Image(pickURL);
         picture.setImage(img);
         root.getChildren().add(picture);
 
-        return root;
+        return root;*/
+        title.setText("Где сам осленок?");
+        zagrImage();
+        title.setFont(Font.font(40));
+        return new VBox(title, picture);
 
     }
+
+    private void initInteraction(Stage primaryStage) {
+        zagrQuest();
+        primaryStage.addEventHandler(MouseEvent.MOUSE_CLICKED, event ->
+                clicked(event.getX(), event.getY())
+        );
+    }
+
+
+    private void zagrImage() {
+        InputStream image = Zachot.class.getResourceAsStream("oselik.jpg")
+        Image img = new Image(image);
+        picture.setImage(img);
+    }
+
+    private void zagrQuest() {
+        InputStream textIS = Zachot.class.getResourceAsStream("qcoord");
+        Scanner scannerIn = new Scanner(textIS);
+        for (int i = 0; i < 5; i++) {
+            questions[i] = new Question(
+                    scannerIn.nextInt(), scannerIn.nextInt(), scannerIn.nextInt(), scannerIn.nextInt(),
+                    scannerIn.nextLine()
+            );
+        }
+    }
+
+    private void clicked(int x, int y) {
+        Question question = questions[questionNumber];
+        if (question.getX() == x && question.getY() == y)
+            result++;
+        questionNumber++;
+
+        if (questionNumber == 5) {
+            new Alert(Alert.AlertType.INFORMATION, "Конец! Итого: " + result + "/" + questionNumber).showAndWait();
+            questionNumber = 0;
+            result = 0;
+        }
+        title.setText(questions[questionNumber].getQuestionString());
+    }
+
 }
